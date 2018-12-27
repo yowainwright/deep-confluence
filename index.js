366f743caecb65a5d399f940868fb114e5412c9f
Jeff Wainwright <yowainwright@gmail.com>
2018-12-26 21:20:13 -0800

    [update] updates notes in `src/index.js` (#38)

--- index.js
+++ index.js
@@ -4,11 +4,11 @@
  * > Deep Confluence is a Javascript Utility Method to deeply merge objects.
  * - meant for: legibility, simplicity, testability, and utility
  * - not meant for: complex {object}/[array] merging with very particular needs which can easily be tested
- * - usecase/audiance: I want to use a bare-bones deep extend method that I can understan
+ * - use-case/audience: I want to use a bare-bones deep extend method that I can understand
  * @param {obj}
  * @param ...args (other {obj}s)
  * */
-function deepConfluence (obj = {}, ...args) {
+export default function deepConfluence (obj = {}, ...args) {
   args.forEach(item => {
     for (const prop in item) {
       const piece = item[prop]
@@ -19,5 +19,3 @@ function deepConfluence (obj = {}, ...args) {
   })
   return obj
 }
-
-export default deepConfluence
