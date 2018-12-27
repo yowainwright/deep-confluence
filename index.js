5128492d6fb901fad74f9e129f297065c596924c
Jeff Wainwright <yowainwright@gmail.com>
2018-12-27 15:01:56 -0800

    [update] adds more checks

--- index.js
+++ index.js
@@ -5,15 +5,29 @@
  * - meant for: legibility, simplicity, testability, and utility
  * - not meant for: complex {object}/[array] merging with very particular needs which can easily be tested
  * - use-case/audience: I want to use a bare-bones deep extend method that I can understand
- * @param {obj}
- * @param ...args (other {obj}s)
  * */
-export default function deepConfluence (obj = {}, ...args) {
+
+
+ /**
+  *
+  * @param {obj} obj
+  */
+const isMergeable = (obj) => Array.isArray(obj) || (obj !== null && typeof obj === 'object' && !(o.then instanceof Function))
+
+/**
+ * @param {obj}
+ * @param {args} array
+*/
+export default function deepConfluence(obj = {}, ...args) {
+  // the initial object must be an object
+  if (Array.isArray(obj) || !isMergeable(obj)) return
   args.forEach(item => {
-    for (const prop in item) {
-      const piece = item[prop]
-      obj[prop] = typeof piece === 'object'
-        ? deepConfluence(obj[prop], piece)
+    // skip items that aren't mergeable
+    if (!isMergeable(item)) continue
+    for (const key in item) {
+      const piece = item[key]
+      obj[key] = typeof piece === 'object'
+        ? deepConfluence(obj[key], piece)
         : piece
     }
   })
