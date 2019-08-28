83570cfc66ee21891047b3bf0e9b855f04feb93f
Jeff Wainwright <yowainwright@gmail.com>
2019-08-28 14:00:37 -0700

    Ts init (#68)
    * [update] adds new lint pkgs
    
    * adds initial ts work
    
    * adds working jest
    
    * adds linting updates

--- dist/deep-confluence.es.js
+++ dist/deep-confluence.es.js
@@ -5,58 +5,41 @@
   @author Jeff Wainwright <yowainwright@gmail.com> (https://jeffry.in)
   @license MIT
 **/
-function _typeof(obj) {
-  if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") {
-    _typeof = function (obj) {
-      return typeof obj;
-    };
-  } else {
-    _typeof = function (obj) {
-      return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj;
-    };
-  }
-
-  return _typeof(obj);
-}
-
 /**
- * # Deep Confluence 🖤
- * ====
- * > Deep Confluence is a Javascript Utility Method to deeply merge objects.
+ * @name Deep Confluence 🖤
+ * @description Deep Confluence is a Javascript Utility Method to deeply merge objects.
  * - meant for: legibility, simplicity, testability, and utility
  * - not meant for: complex {object}/[array] merging with very particular needs which can easily be tested
  * - use-case/audience: I want to use a bare-bones deep extend method that I can understand
  */
-
 /**
-  *
+  * @name isMergeable
   * @param {obj} obj
   */
-var isMergeable = function isMergeable(obj) {
-  return obj !== null && _typeof(obj) === 'object' && !(obj.then instanceof Function);
-};
+var isMergeable = function (obj) { return obj !== null && typeof obj === 'object' && !(obj.then instanceof Function); };
 /**
+ * @name deepConfluence
  * @param {obj}
- * @param {args} array
+ * @param {args} obj(s)
  */
-
-
-function deepConfluence() {
-  var obj = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-  // the initial object must be an object
-  if (!isMergeable(obj)) return;
-
-  for (var _len = arguments.length, args = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-    args[_key - 1] = arguments[_key];
-  }
-
-  args.forEach(function (item) {
-    for (var key in item) {
-      var itemProperty = item[key];
-      obj[key] = _typeof(itemProperty) === 'object' ? deepConfluence(obj[key], itemProperty) : itemProperty;
+function deepConfluence(obj) {
+    if (obj === void 0) { obj = {}; }
+    var args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        args[_i - 1] = arguments[_i];
     }
-  });
-  return obj;
+    // the initial object must be an object
+    if (!isMergeable(obj))
+        return;
+    args.forEach(function (item) {
+        for (var key in item) {
+            var itemProperty = item[key];
+            obj[key] = typeof itemProperty === 'object'
+                ? deepConfluence(obj[key], itemProperty)
+                : itemProperty;
+        }
+    });
+    return obj;
 }
 
 export default deepConfluence;
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
 /**
- * # Deep Confluence 🖤
- * ====
- * > Deep Confluence is a Javascript Utility Method to deeply merge objects.
+ * @name Deep Confluence 🖤
+ * @description Deep Confluence is a Javascript Utility Method to deeply merge objects.
  * - meant for: legibility, simplicity, testability, and utility
  * - not meant for: complex {object}/[array] merging with very particular needs which can easily be tested
  * - use-case/audience: I want to use a bare-bones deep extend method that I can understand
  */
 
+type $FIXME = any
 /**
-  *
+  * @name isMergeable
   * @param {obj} obj
   */
-const isMergeable = (obj) => obj !== null && typeof obj === 'object' && !(obj.then instanceof Function)
+
+const isMergeable = (obj: $FIXME) => obj !== null && typeof obj === 'object' && !(obj.then instanceof Function)
 
 /**
+ * @name deepConfluence
  * @param {obj}
- * @param {args} array
+ * @param {args} obj(s)
  */
-export default function deepConfluence (obj = {}, ...args) {
+export default function deepConfluence (obj: $FIXME = {}, ...args: $FIXME) {
   // the initial object must be an object
   if (!isMergeable(obj)) return
-  args.forEach(item => {
+  args.forEach((item: $FIXME) => {
     for (const key in item) {
       const itemProperty = item[key]
       obj[key] = typeof itemProperty === 'object'
