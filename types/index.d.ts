2ba70588f2e339778afbcabf5a5d686bd184e616
Jeff Wainwright <yowainwright@gmail.com>
2019-08-28 13:55:31 -0700

    adds working jest

---
+++ types/index.d.ts
@@ -0,0 +1,15 @@
+/**
+ * @name Deep Confluence 🖤
+ * @description Deep Confluence is a Javascript Utility Method to deeply merge objects.
+ * - meant for: legibility, simplicity, testability, and utility
+ * - not meant for: complex {object}/[array] merging with very particular needs which can easily be tested
+ * - use-case/audience: I want to use a bare-bones deep extend method that I can understand
+ */
+declare type $FIXME = any;
+/**
+ * @name deepConfluence
+ * @param {obj}
+ * @param {args} obj(s)
+ */
+export default function deepConfluence(obj?: $FIXME, ...args: $FIXME): any;
+export {};
