9dda9a6ed93cdf0f2f8ad8747c8721c7c9fe3c3f
Jeff Wainwright <yowainwright@gmail.com>
2021-09-10 10:59:28 -0700

    fix: fixes prototype pollution issue, I think 🙏 (#221)

--- dist/deep-confluence.es.js
+++ dist/deep-confluence.es.js
@@ -1,25 +1,26 @@
 /**
   deep-confluence - Merge objects—deeply 🖤
-  @version v1.2.2
+  @version v1.2.3
   @link https://github.com/yowainwright/deep-confluence#readme
   @author Jeff Wainwright <yowainwright@gmail.com> (https://jeffry.in)
   @license MIT
 **/
 /*! *****************************************************************************
-Copyright (c) Microsoft Corporation. All rights reserved.
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-this file except in compliance with the License. You may obtain a copy of the
-License at http://www.apache.org/licenses/LICENSE-2.0
+Copyright (c) Microsoft Corporation.
 
-THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
-WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
-MERCHANTABLITY OR NON-INFRINGEMENT.
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted.
 
-See the Apache Version 2.0 License for specific language governing permissions
-and limitations under the License.
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+PERFORMANCE OF THIS SOFTWARE.
 ***************************************************************************** */
 
+/** @deprecated */
 function __spreadArrays() {
     for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
     for (var r = Array(s), k = 0, i = 0; i < il; i++)
@@ -56,7 +57,10 @@ function deepConfluence(item, otherItem) {
     if (isArray(item) && isArray(otherItem))
         return filterArray(__spreadArrays(item, otherItem));
     return filterArray(__spreadArrays(Object.keys(item), Object.keys(otherItem))).reduce(function (acc, key) {
-        if (typeof acc[key] === 'undefined') {
+        if (key === '__proto__') {
+            acc = otherItem;
+        }
+        else if (typeof acc[key] === 'undefined') {
             acc[key] = otherItem[key];
         }
         else if (isObject(acc[key]) || isArray(acc[key])) {
